96897d2b3e010977f34c785e3cc017c00c99cefd
Intan Melinda <intanmelinda23@gmail.com>
2024-05-17 00:20:30 +0700

    first commit

---
+++ application/views/side_menu.php
@@ -0,0 +1,7 @@
+<ul data-role="listview">
+    <li data-icon="delete"><a href="#" data-rel="close">Close menu</a></li>
+    <li><a href="<?PHP echo base_url('index.php');?>">Dasboard</a></li>
+    <li><a href="<?PHP echo base_url('index.php/C_barang_group');?>">Barang</a></li>
+    <li><a href="<?PHP echo base_url('index.php/C_anggota');?>">Anggota</a></li>
+    <li><a href="<?PHP echo base_url('index.php/C_karyawan');?>">Karyawan</a></li>
+</ul>
